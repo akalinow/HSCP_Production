dff1a617246b87c64636c40ce144ccabf785aab0
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2021-04-30 09:23:02 +0200

    * use relative path for links

--- Analysis_TOFUtility.h
+++ Analysis_TOFUtility.h
@@ -1,1 +1,1 @@
-/home/akalinow/scratch//CMS/HSCP/CMSSW_10_6_20/src/SUSYBSMAnalysis/HSCP/test/AnalysisCode/Analysis_TOFUtility.h
+../CMSSW_10_6_12/src/SUSYBSMAnalysis/HSCP/test/AnalysisCode/Analysis_TOFUtility.h
--- GeomDumpConfig_cfg.py
+++ GeomDumpConfig_cfg.py
@@ -1,1 +1,1 @@
-/home/akalinow/scratch//CMS/HSCP/CMSSW_10_6_20/src/SUSYBSMAnalysis/HSCP/test/UsefulScripts/DumpGeometry/GeomDumpConfig_cfg.py
+../CMSSW_10_6_12/src/SUSYBSMAnalysis/HSCP/test/UsefulScripts/DumpGeometry/GeomDumpConfig_cfg.py
