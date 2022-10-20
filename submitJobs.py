1d03f2fc0a493d090b87f3f47e2f574959e4ace6
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2022-10-20 13:47:10 +0200

    * add config for a full simulation of PhaseII detector

--- submitJobs.py
+++ submitJobs.py
@@ -21,6 +21,11 @@ def createGenSimFramgent(era, withPileUp, generator_fragment):
          premix_switches = "--step GEN,SIM,DIGI,DATAMIX,L1,DIGI2RAW "
          premix_switches += pileup_inputs[era]+" "
          premix_switches += "--procModifiers premix_stage2 --datamix PreMix "
+
+    if era=="29":
+        premix_switches = "--step GEN,SIM,DIGI,L1TrackTrigger,L1,HLT,L1Reco,RECO,RECOSIM "
+        premix_switches += "--customise SLHCUpgradeSimulations/Configuration/aging.customise_aging_1000 "
+        premix_switches += "--customise Configuration/DataProcessing/Utils.addMonitoring "     
         
     command = "cmsDriver.py " +\
     "Configuration/GenProduction/python/ThirteenTeV/HSCP/"+generator_fragment+" "\
@@ -29,10 +34,8 @@ def createGenSimFramgent(era, withPileUp, generator_fragment):
     cmssw8_fix +\
     premix_switches +\
     eras_conditions[era] +" "+\
-    "--geometry DB:Extended "+\
     "--customise SimG4Core/CustomPhysics/Exotica_HSCP_SIM_cfi.customise "+\
-    "--nThreads 4 "+\
-    "--runUnscheduled "+\
+    "--nThreads 1 "+\
     "--python_filename GEN_SIM_DIGI_RAW_step_cfg.py -n 2 --no_exec"
     print command
     os.system(command)
@@ -103,12 +106,14 @@ def prepareCrabCfg(era,
 #https://twiki.cern.ch/twiki/bin/view/CMS/PdmVRun2LegacyAnalysisSummaryTable
 #########################################
 eras_conditions = {
-    "18":"--era Run2_2018 --conditions 106X_upgrade2018_realistic_v15_L1v1 --beamspot Realistic25ns13TeVEarly2018Collision ",
-    "17":"--era Run2_2017 --conditions 106X_mc2017_realistic_v8 --beamspot Realistic25ns13TeVEarly2017Collision ",
-    "16":"--era Run2_2016 --conditions 106X_mcRun2_asymptotic_v15 --beamspot Realistic25ns13TeV2016Collision "
+    "29":"--era Phase2C9  --conditions 123X_mcRun4_realistic_v3 --geometry Extended2026D49",
+    "18":"--era Run2_2018 --conditions 106X_upgrade2018_realistic_v15_L1v1  --geometry Extended2026D49 --beamspot Realistic25ns13TeVEarly2018Collision ",
+    "17":"--era Run2_2017 --conditions 106X_mc2017_realistic_v8 --geometry Extended2026D49 --beamspot Realistic25ns13TeVEarly2017Collision ",
+    "16":"--era Run2_2016 --conditions 106X_mcRun2_asymptotic_v15 --geometry Extended2026D49 --beamspot Realistic25ns13TeV2016Collision "
     }
 
 pileup_inputs = {
+    "29":" ",
     "18":"--pileup_input \"dbs:/Neutrino_E-10_gun/RunIISummer19ULPrePremix-UL18_106X_upgrade2018_realistic_v11_L1v1-v2/PREMIX\" ",
     "17":"--pileup_input \"dbs:/Neutrino_E-10_gun/RunIISummer19ULPrePremix-UL17_106X_mc2017_realistic_v6-v1/PREMIX\" ",
     "16":"--pileup_input \"dbs:/Neutrino_E-10_gun/RunIISummer19ULPrePremix-UL16_106X_mcRun2_asymptotic_v10-v2/PREMIX\" "
@@ -120,15 +125,16 @@ generator_fragments = [aFile.split("/")[-1] for aFile in glob.glob(genFragmentsD
 
 ##Those are the steering parameters
 generator_fragments = ["HSCPstop_M_800_TuneCP5_13TeV_pythia8_cff.py",
-                       "HSCPstoponlyneutral_M_400_TuneCP5_13TeV_pythia8_cff.py"]
-era = "17"
-eventsPerJob = 200
-numberOfJobs = 2
+                       #"HSCPstoponlyneutral_M_400_TuneCP5_13TeV_pythia8_cff.py"
+]
+era = "29"
+eventsPerJob = 10
+numberOfJobs = 1
 outLFNDirBase = "/store/user/akalinow/HSCP/"
 storage_element="T2_PL_Swierk"
 outputDatasetTag = "test9"
 withPileUp = False
-runLocal = False
+runLocal = True
 ########################################################
 for aFragment in generator_fragments:
     prepareCrabCfg(era = era,
